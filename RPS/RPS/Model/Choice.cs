81d4112c238fb804779d63c8b64f053b54a9376f
MichalPetricek <57211483+MichalPetricek@users.noreply.github.com>
2020-05-09 17:59:27 +0200

    Add files via upload

---
+++ RPS/RPS/Model/Choice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RPS.Model
+{
+    public enum Choice
+    {
+        None,
+        Rock,
+        Paper,
+        Scissors
+    
+    }
+}
